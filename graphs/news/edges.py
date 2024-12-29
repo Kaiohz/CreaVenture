c29fd617372cf7b4799db5acd6bf787c76a8abe6
Kaiohz <goncalves.yohan@gmail.com>
2024-12-29 18:23:59 +0100

    wip pytube

--- graphs/news/edges.py
+++ graphs/news/edges.py
@@ -1,13 +1,6 @@
 from typing import List
-from graphs.news.chains.fetch_news import FetchNewsChain
-from graphs.news.chains.generate import GenerateChain
-from graphs.news.chains.country_finder import CountryFinderChain
 from graphs.news.chains.headlines_or_specific import HeadlinesOrSpecificChain
-from graphs.news.chains.topic_finder import TopicFinderChain
 from graphs.news.models.choice import ChoiceModel
-from graphs.news.models.country import CountryModel
-from graphs.news.models.topic import TopicModel
-from graphs.news.state import NewsState
 from graphs.graph_params import GraphParams
 from prompts.prompt_loader import PromptLoader
 
