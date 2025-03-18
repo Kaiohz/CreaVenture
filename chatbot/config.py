4243630c048354587da58e8c8a2f0028c0e0aa67
Kaiohz <goncalves.yohan@gmail.com>
2025-03-18 13:48:45 +0700

    remove useless vars in config, maj example

--- chatbot/config.py
+++ chatbot/config.py
@@ -3,15 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-class SerpConfig:
-    API_KEY = os.getenv("SERP_API_KEY")
-
-
 class GoogleNewsConfig:
     BASE_URL = os.getenv("GOOGLE_NEWS_BASE_URL")
     API_KEY = os.getenv("GOOGLE_NEWS_API_KEY")
-
-
-class GeminiConfig:
-    API_KEY = os.getenv("GOOGLE_API_KEY")
